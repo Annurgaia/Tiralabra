33f774ad103cb73257696f16fda3328ec144e761
Anna Hietanen <annahietanen@Annas-MacBook-Pro.local>
2012-09-14 01:24:08 +0300

    Fixing the tests

--- Binary/test/main/BinomialTest.java
+++ Binary/test/main/BinomialTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 
@@ -13,6 +9,7 @@ import org.junit.*;
  * @author annahietanen
  */
 public class BinomialTest {
+    
     Binomial heap;
     public BinomialTest() {
     }
