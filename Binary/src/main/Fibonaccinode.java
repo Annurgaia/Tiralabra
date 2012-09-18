f87ef579d35f95e1ac42c25966a9e33705a0d0e1
Anna Hietanen <annahietanen@Annas-MacBook-Pro.local>
2012-09-18 13:38:52 +0300

    Fibonaccia aloiteltu

---
+++ Binary/src/main/Fibonaccinode.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author annahietanen
+ */
+public class Fibonaccinode {
+ int value;
+    int degree;
+    boolean mark;
+    Fibonaccinode child;
+    Fibonaccinode left;
+    Fibonaccinode right;
+    Fibonaccinode parent;
+
+    public Fibonaccinode(int value){
+        this.value = value;
+    }
+
+}
